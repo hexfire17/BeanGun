fef4bf39afd07c325df35b9ea5fa795ed1635b8c
hexfire17 <hexfire17@gmail.com>
2016-04-17 18:39:07 -0400

    gameover screen working

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		FindObjectOfType<Player> ().onDeath += GameOver;
+	}
+
+	void GameOver ()
+	{
+		StartCoroutine (Fade (Color.clear, Color.black, 1));
+	}
+
+	IEnumerator Fade (Color fadeFrom, Color fadeTo, float time)
+	{
+		float speed = 1 / time;
+		float percent = 0;
+
+		while (percent < 1) {
+			percent += Time.deltaTime * speed;
+			_fadePlane.color = Color.Lerp (fadeFrom, fadeTo, percent);
+			yield return null;
+		}
+		_gameOverUI.SetActive (true);
+	}
+
+	public Image _fadePlane;
+	public GameObject _gameOverUI;
+}
