aec64449fc5c46fd598c3a4b0ed4cae58e80e1a1
hexfire17 <hexfire17@gmail.com>
2016-04-26 19:35:35 -0400

    trying to add a joystick, not totally working, I have it up, but can't click it, also need a way to add it into the game and enable/disable it. Also had the idea of having two joysticks one on each side for easier gameplay, but you can still shoot by clicking, mayber even have an option to use one stick for shooting, or nboth for walkking and screen for shots

---
+++ Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+	void Start ()
+	{
+		_backgroundImage = GetComponent<Image> ();
+		_stickImage = transform.GetChild (0).GetComponent<Image> ();
+	}
+
+	public virtual void OnDrag (PointerEventData ped)
+	{
+		Vector2 position;
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_backgroundImage.rectTransform, ped.position, ped.pressEventCamera, out position))
+		{
+			position.x = (position.x / _backgroundImage.rectTransform.sizeDelta.x);
+			position.y = (position.x / _backgroundImage.rectTransform.sizeDelta.y);
+
+			_inputVector = new Vector3 (position.x * 2 + 1, 0, position.y * 2 - 1);
+			if (_inputVector.magnitude > 1f)
+			{
+				_inputVector = _inputVector.normalized;
+			}
+
+			_stickImage.rectTransform.anchorMax = new Vector3 (_inputVector.x * (_backgroundImage.rectTransform.sizeDelta.x / 2),
+				_backgroundImage.rectTransform.sizeDelta.y / 2);
+			Debug.Log(position);
+		}
+	}
+
+	public virtual void OnPointerDown (PointerEventData ped)
+	{
+		OnDrag (ped);
+	}
+
+	public virtual void OnPointerUp (PointerEventData ped)
+	{
+		_inputVector = Vector3.zero;
+		_stickImage.rectTransform.anchoredPosition = Vector3.zero;
+	}
+
+	public Vector2 getInputVector ()
+	{
+		return _inputVector;
+	}
+
+	private Image _backgroundImage;
+	private Image _stickImage;
+
+	private Vector3 _inputVector;
+}
