cbd75a15602dca39fb045d76e64e440d999c881f
hexfire17 <hexfire17@gmail.com>
2016-04-29 10:55:00 -0400

    multi-muzzle working

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,25 +6,46 @@ public class Gun : MonoBehaviour
 	void Start ()
 	{
 		_muzzleFlash = GetComponent <MuzzleFlash> ();
+		_currBurst = 0;
 	}
 
 	public void Shoot()
 	{
-		if (Time.time > _nextShotTime) 
+		if (Time.time > _nextShotTime)
 		{
-			_nextShotTime = Time.time + _millisBetweenShots / 1000;
-			Projectile projectile = Instantiate (_projectile, _muzzle.position, _muzzle.rotation) as Projectile;
-			projectile.SetSpeed (_projectileVelocity);
+			for (int i = 0; i < _projectileSpawns.Length; i++) 
+			{
+				Transform currentSpawn = _projectileSpawns [i];
+				Projectile projectile = Instantiate (_projectile, currentSpawn.position, currentSpawn.rotation) as Projectile;
+				projectile.SetSpeed (_projectileVelocity);
 
+				// Configure next shots
+				_nextShotTime = Time.time + _millisBetweenShots / 1000;
+				if (_fireMode == FireMode.Burst) {
+					_currBurst++;
+					if (_currBurst >= _maxBurt) {
+						_currBurst = 0;
+						_nextShotTime += _timeBetweenBurst;
+					}
+				}
+			}
+			// only do this for one muzzle TODO check if this is crazy to have a lot
 			_muzzleFlash.Activate ();
 			Instantiate (_shell, _shellEjectionPoint.position, _shellEjectionPoint.rotation);
 		}
-	}
+    }
+
+	public enum FireMode {Auto, Burst};
+	public FireMode _fireMode;
+
+	int _maxBurt = 3;
+	int _currBurst;
+	float _timeBetweenBurst = .5f;
 
 	float _nextShotTime;
 	MuzzleFlash _muzzleFlash;
 
-	public Transform _muzzle;
+	public Transform[] _projectileSpawns;
 	public Projectile _projectile;
 	public float _millisBetweenShots = 100;
 	public float _projectileVelocity = 35;
