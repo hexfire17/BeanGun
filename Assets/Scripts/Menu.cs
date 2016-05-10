ef40e42cb51eae61aa0c8abf52a7a0c263386783
hexfire17 <hexfire17@gmail.com>
2016-05-10 18:59:23 -0400

    upgrade slide working

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -23,17 +23,48 @@ public class Menu : MonoBehaviour {
 
 	void Open ()
 	{
-		Debug.Log ("opening");
+		StartCoroutine ("AnnimateOpen");
+	}
+
+	IEnumerator AnnimateOpen ()
+	{
+		float speed = 10f;
+		float annimatePercent = 0f;
+		while (annimatePercent <= 1) 
+		{
+			annimatePercent += Time.deltaTime * speed;
+			float yVal = Mathf.Lerp (800, -1100, annimatePercent);
+			Vector2 modifiedVector = new Vector2 (_menuBox.anchoredPosition.x, yVal);
+			_menuBox.anchoredPosition = modifiedVector;
+			yield return null;
+		}
+
 		Time.timeScale = 0f;
 	}
 
 	void Close ()
 	{
-		Debug.Log ("closing");
+		StartCoroutine ("AnnimateClose");
+	}
+
+	IEnumerator AnnimateClose ()
+	{
 		Time.timeScale = 1f;
+
+		float speed = 7f;
+		float annimatePercent = 0f;
+		while (annimatePercent <= 1) 
+		{
+			annimatePercent += Time.deltaTime * speed;
+			float yVal = Mathf.Lerp (-1100, -5000, annimatePercent);
+			Vector2 modifiedVector = new Vector2 (_menuBox.anchoredPosition.x, yVal);
+			_menuBox.anchoredPosition = modifiedVector;
+			yield return null;
+		}
 	}
 
 	bool _menuOpen;
+	public RectTransform _menuBox;
 
 	// SetUserPrefs
 }
