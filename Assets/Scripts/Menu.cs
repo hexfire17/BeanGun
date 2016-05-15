dcb7efa65ccd038b86d0aea2a139e72e95aff4ba
Conti <conti@amazon.com>
2016-05-15 12:37:20 -0400

    check

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,28 +8,48 @@ public class Menu : MonoBehaviour {
 	{
 		_menuOpen = false;
 		_player = FindObjectOfType<Player> ();
+
+		_shootSpeedBar.Init (20,10);
+		_moveSpeedBar.Init (20, 1);
+		_damageBar.Init (20, 1);
 	}
 
 	public void IncrementShootSpeed ()
 	{
 		_player.GetComponent<GunController> ()._equippedGun._projectileVelocity++;
 		_player.GetComponent<GunController> ()._equippedGun._millisBetweenShots--;
+		_shootSpeedBar.Add (1);
 	}
 
 	public void DecrementShootSpeed ()
 	{
 		_player.GetComponent<GunController> ()._equippedGun._projectileVelocity--;
 		_player.GetComponent<GunController> ()._equippedGun._millisBetweenShots++;
+		_shootSpeedBar.Subtract (1);
 	}
 
 	public void IncrementDamage ()
 	{
 		_player.GetComponent<GunController> ()._equippedGun._gunDamage++;
+		_damageBar.Add (1);
 	}
 
 	public void DecrementDamage ()
 	{
 		_player.GetComponent<GunController> ()._equippedGun._gunDamage++;
+		_damageBar.Subtract (1);
+	}
+
+	public void IncrementMoveSpeed ()
+	{
+		_moveSpeedBar.Add (1);
+		_player._moveSpeed = _player._moveSpeed + 1;
+	}
+
+	public void DecrementMoveSpeed ()
+	{
+		_moveSpeedBar.Subtract (1);
+		_player._moveSpeed = _player._moveSpeed - 1;
 	}
 
 	public void OnArrowClick ()
@@ -89,6 +109,10 @@ public class Menu : MonoBehaviour {
 	bool _menuOpen;
 	public RectTransform _menuBox;
 	public Player _player;
+
+	public StatBar _shootSpeedBar;
+	public StatBar _damageBar;
+	public StatBar _moveSpeedBar;
 
 	// SetUserPrefs
 }
