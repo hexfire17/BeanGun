2db7012528f1435d713eec4683774092d6e31508
Conti <conti@amazon.com>
2016-05-15 11:25:38 -0400

    sub working

--- Assets/Scripts/StatBar.cs
+++ Assets/Scripts/StatBar.cs
@@ -11,24 +11,10 @@ public class StatBar : MonoBehaviour {
 		Debug.Log ("StartSize: " + _valueRectStartLength);
 	}
 
-	public void Add(float points)
-	{
-		Debug.Log ("PointAdd: " + points);
-		float percentLost = points / _maxValue;
-		Debug.Log ("Plost: " + percentLost);
-		float sizeMinus = _valueRectStartLength * percentLost;
-		Debug.Log ("SizeMinus: " + sizeMinus);
-		_fill.sizeDelta -= new Vector2 (sizeMinus, 0);
-		_fill.anchoredPosition -= new Vector2 (sizeMinus, 0) * .5f;
-	}
-
 	public void Subtract(float points)
 	{
-		Debug.Log ("PointSub: " + points);
 		float percentLost = points / _maxValue;
-		Debug.Log ("Plost: " + percentLost);
 		float sizeMinus = _valueRectStartLength * percentLost;
-		Debug.Log ("SizeMinus: " + sizeMinus);
 		_fill.sizeDelta -= new Vector2 (sizeMinus, 0);
 		_fill.anchoredPosition -= new Vector2 (sizeMinus, 0) * .5f;
 	}
